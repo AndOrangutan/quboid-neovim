e97d0cf5dbd1508f21b9721ca9711207526241dd
Von Mueller <vonjonmueller@gmail.com>
2026-01-11 15:56:34 -0800

    Add config autocmds

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,65 @@
+-- This autocmd will only trigger when a file was loaded from the cmdline.
+-- It will render the file as quickly as possible.
+vim.api.nvim_create_autocmd("BufReadPost", {
+    once = true,
+    callback = function(event)
+        -- Skip if we already entered vim
+        if vim.v.vim_did_enter == 1 then
+            return
+        end
+
+        -- Try to guess the filetype (may change later on during Neovim startup)
+        local ft = vim.filetype.match({ buf = event.buf })
+        if ft then
+            -- Add treesitter highlights and fallback to syntax
+            local lang = vim.treesitter.language.get_lang(ft)
+            if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
+                vim.bo[event.buf].syntax = ft
+            end
+
+            -- Trigger early redraw
+            vim.cmd([[redraw]])
+        end
+    end,
+})
+
+-- Automattically create missing subdirectories
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+    pattern = "*",
+    callback = function(event)
+        local dir = vim.fn.fnamemodify(event.file, ":p:h")
+        if vim.fn.isdirectory(dir) == 0 then
+            vim.fn.mkdir(dir, "p")
+        end
+    end
+})
+
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
+})
+
+-- removes trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        local save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
+
+-- highlights yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 40,
+        })
+    end,
+})
