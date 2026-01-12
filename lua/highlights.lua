bc36a5c6e78fdeeebc6306d8396a25db83fb8355
Von Mueller <vonjonmueller@gmail.com>
2026-01-11 16:20:43 -0800

    Add highlights autocmd enabling cust hl

---
+++ lua/highlights.lua
@@ -0,0 +1,20 @@
+local defaults = require('defaults')
+
+local highlights = vim.api.nvim_create_augroup('highlights', { clear = true }}
+vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = '*',
+    group = highlights,
+    callback = function()`
+        -- Rebind float
+        vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Pmenu' })
+
+        -- Requires `vim.opt.cursorline = true`
+        vim.api.nvim_set_hl(0, "CursorLine", { link = "NONE" })
+
+        -- vim.api.nvim_set_hl(0, 'WinBar', { link = "Normal" })
+        -- vim.api.nvim_set_hl(0, 'WinBarNC', { link = "Normal" })
+    end,
+})
+
+vim.opt.background = defaults.background
+vim.cmd('colorscheme ' .. defaults.colorscheme)
