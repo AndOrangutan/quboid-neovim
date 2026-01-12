99d912e7afb4a7ae25c7571d18456ea9d8f5e5f6
Von Mueller <vonjonmueller@gmail.com>
2026-01-11 16:16:43 -0800

    Add defaults

---
+++ lua/defaults.lua
@@ -0,0 +1,37 @@
+local _M = {}
+
+----------------
+-- Exclusions --
+----------------
+
+_M.ft_exclude = {
+    '', -- disable for empty buffer
+    'fugitive',
+    'NeogitCommitMessage',
+    'NeogitDiffView',
+    'NeogitStatus',
+}
+
+_M.bt_exclude = {
+    'nofile',
+    'quickfix',
+    'prompt',
+}
+
+---------
+-- GUI --
+---------
+_M.background = "dark"
+_M.border = {
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+    {" ", "FloatBorder"},
+}
+_M.colorscheme = "default"
+
+return _M
