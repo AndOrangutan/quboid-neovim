b59b19c4b9923714c5340d20497d24f1d9590651
Von Mueller <vonjonmueller@gmail.com>
2026-03-05 17:44:40 -0800

    Fix centering of smooth scroll bindings

--- lua/plugins/bloat.lua
+++ lua/plugins/bloat.lua
@@ -10,7 +10,13 @@ return {
             lazy = true,
             after = function ()
                 require('mini.animate').setup()
-            end
+            end,
+            keys = {
+                { lhs = 'n', rhs = '<Cmd>lua vim.cmd("normal! n"); ' .. 'MiniAnimate.execute_after("scroll", "normal! zvzz")<CR>' , desc = 'Smooth Center [n]ext' },
+                { lhs = 'N', rhs = '<Cmd>lua vim.cmd("normal! N"); ' .. 'MiniAnimate.execute_after("scroll", "normal! zvzz")<CR>' , desc = 'Smooth Center Prev' },
+                { lhs = '<C-u>', rhs = '<Cmd>lua vim.cmd("normal! <C-u>"); ' .. 'MiniAnimate.execute_after("scroll", "normal! zvzz")<CR>' , desc = 'Smooth Center Scroll Up' },
+                { lhs = '<C-d>', rhs = '<Cmd>lua vim.cmd("normal! <C-d>"); ' .. 'MiniAnimate.execute_after("scroll", "normal! zvzz")<CR>' , desc = 'Smooth Center Scroll Down' },
+            },
         },
     },
 }
